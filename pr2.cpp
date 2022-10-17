b2c1d2adced85669719fe764951bb53ba96670df
OlesMak24 <115488772+OlesMak24@users.noreply.github.com>
2022-10-18 01:16:23 +0300

    коментар №1

--- pr2.cpp
+++ pr2.cpp
@@ -15,7 +15,7 @@ int main()
 	double z1; // результат обчислення 1-го виразу
 	double z2; // результат обчислення 2-го виразу
 	cout << "a = "; cin >> a;
-
+//(2a - false)(2 * a - true)
 	z1 = (sin(2 * a) + sin(5 * a) - sin(3 * a)) / (cos(a) + 1 - 2 * (sin(2 * a) * sin(2 * a)));
 	z2 = 2 * (sin(a));
 
